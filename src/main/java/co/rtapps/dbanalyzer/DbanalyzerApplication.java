06892105461260b677222939d1fcf22d2aaef867
rodrigo torres <rodrigo.torres@rtorres-ltm.internal.salesforce.com>
2021-08-30 08:51:31 -0400

    first commit

---
+++ src/main/java/co/rtapps/dbanalyzer/DbanalyzerApplication.java
@@ -0,0 +1,13 @@
+package co.rtapps.dbanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbanalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbanalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/rtapps/dbanalyzer/DbanalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.rtapps.dbanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbanalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
